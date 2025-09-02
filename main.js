ca2ef5db3f85f4f6d225ddb23e62e0742ae15f95
funnelidsilva-boop <funnelidsilva@gmail.com>
2025-09-02 23:26:08 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+// main.js
+import { initUI } from './ui.js';
+import './gallery.js';   // sets up its own events
+import './cart.js';      // sets up cart events
+import './checkout.js';  // sets up checkout events
+
+initUI();
